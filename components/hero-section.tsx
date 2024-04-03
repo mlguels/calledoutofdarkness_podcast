d197e8acd8552cfa38833d46d0f4f4d4934edfc9
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-04-03 14:39:57 -0700

    Fix hero section layout issues

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -10,53 +10,41 @@ export const pinyonScript = Pinyon_Script({
 
 export default function HeroSection() {
   return (
-    <section className="flex flex-col md:flex-row justify-between w-auto gap-20">
+    <section className="flex flex-col md:flex-row justify-between gap-10 p-20">
       <ul className="flex flex-col">
         <li>
-          <h2 className="text-8xl font-marxiana text-[#794F42] tracking-tighter">
-            Called
-          </h2>
+          <h2 className="heading-2">Called</h2>
         </li>
         <li>
-          <h2 className="text-8xl font-marxiana text-[#794F42] tracking-tighter">
-            Out of
-          </h2>
+          <h2 className="heading-2">Out of</h2>
         </li>
         <li>
-          <h2 className="text-8xl font-marxiana text-[#794F42] tracking-tighter">
-            Darkness
-          </h2>
+          <h2 className="heading-2">Darkness</h2>
         </li>
-        <li>
-          <div className="flex flex-col w-full">
-            <h2 className="text-7xl font-marxiana text-cream self-end">
-              Summer
-            </h2>
+        <li className="flex flex-col">
+          <h2 className="text-7xl font-marxiana text-cream self-end">Summer</h2>
 
-            <p
-              className={`${pinyonScript.className} text-7xl text-[#794F42] flex justify-between`}
-            >
-              with
-              <span>
-                {" "}
-                <h2 className="text-7xl font-marxiana text-cream self-end">
-                  Sollis
-                </h2>
-              </span>
-            </p>
-          </div>
-          <li className="mt-6">
+          <p
+            className={`${pinyonScript.className} text-7xl text-brown-secondary flex justify-between`}
+          >
+            with
+            <span className="text-7xl font-marxiana text-cream self-end">
+              {" "}
+              Sollis
+            </span>
+          </p>
+          <div className="w-full mt-5">
             <StreamButton />
-          </li>
+          </div>
         </li>
       </ul>
 
       <Image
         src="/images/summer.png"
         alt="Picture of the author"
-        width={500}
-        height={200}
-        className="w-full h-full"
+        width={100}
+        height={100}
+        className="w-full h-full max-w-[500px] max-h-[500px]  rounded-2xl"
       />
     </section>
   );
