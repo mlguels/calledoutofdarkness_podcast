b0978dbddd3cb4365f03d6c966a320d5cdb8c103
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-04-04 22:33:01 -0700

    Changed font

--- components/stream-btn.tsx
+++ components/stream-btn.tsx
@@ -1,9 +1,12 @@
+import { lora } from "@/app/layout";
 import Link from "next/link";
 
 export default function StreamButton() {
   return (
     <Link href="/">
-      <button className="uppercase bg-green text-[10px] md:text-lg px-auto md:px-8 py-3 md:py-4 rounded-lg md:rounded-2xl border-black border-[1px] whitespace-nowrap w-full">
+      <button
+        className={`${lora.className} uppercase bg-green text-[10px] md:text-lg px-auto md:px-8 py-3 md:py-4 rounded-lg md:rounded-2xl border-black border-[1px] whitespace-nowrap w-full`}
+      >
         Stream the latest episode here
       </button>
     </Link>
