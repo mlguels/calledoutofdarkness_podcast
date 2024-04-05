fc0cdc43b5196fbdb4ab80ef0896ac6eda455196
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-04-05 16:00:21 -0700

    Fix color

--- components/stream-btn.tsx
+++ components/stream-btn.tsx
@@ -5,7 +5,7 @@ export default function StreamButton() {
   return (
     <Link href="/">
       <button
-        className={`${lora.className} uppercase bg-green text-[10px] md:text-lg px-auto md:px-8 py-3 md:py-4 rounded-lg md:rounded-2xl border-black border-[1px] whitespace-nowrap w-full`}
+        className={`${lora.className} uppercase bg-green-primary text-[10px] md:text-lg px-auto md:px-8 py-3 md:py-4 rounded-lg md:rounded-2xl border-black border-[1px] whitespace-nowrap w-full`}
       >
         Stream the latest episode here
       </button>
