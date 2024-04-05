22f8a144e651168655e0de196dd2d2f115b34258
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-04-05 16:00:40 -0700

    Created social links comp

---
+++ components/socials.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { socialLinks } from "@/constant/data";
+import { lora } from "@/app/layout";
+
+export default function Socials() {
+  return (
+    <main className="flex gap-4">
+      <h1 className={`${lora.className} text-3xl`}>Socials</h1>
+      <ul className="flex gap-2">
+        {socialLinks.map((social, i) => (
+          <li key={i}>
+            <Link href={social.link}>
+              <Image
+                src={social.icon}
+                alt="social icon"
+                width={40}
+                height={25}
+              />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
