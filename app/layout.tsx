318419fe8d24488774789d6aede5819cc1a52104
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-04-05 23:00:56 -0700

    Create footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Lora } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/footer";
 
 export const inter = Inter({ subsets: ["latin"] });
 export const lora = Lora({ subsets: ["latin"] });
@@ -17,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${inter.className}`}>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
