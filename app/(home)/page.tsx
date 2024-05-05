6f87e72c3ca17ff62c363dde0ec483e7d8919a17
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-05-04 17:05:11 -0700

    Fix deployment issues

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,7 @@
 import AboutMe from "@/components/about-section";
 import ContactPage from "@/components/contact-page";
 import Footer from "@/components/footer";
-import GuestSpeaker from "@/components/guest-speaker";
 import HeroSection from "@/components/hero-section";
-import MonthlyGuests from "@/components/monthly-guests";
 import PodcastDesc from "@/components/podcast-desc";
 
 export default function Home() {
@@ -12,8 +10,6 @@ export default function Home() {
       <HeroSection />
       <PodcastDesc />
       <AboutMe />
-      {/* <MonthlyGuests /> */}
-      {/* <GuestSpeaker /> */}
       <ContactPage />
       <Footer />
     </main>
