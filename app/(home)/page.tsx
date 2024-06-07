6ff3589cd0e24b794bc0c73190117f80f0a7a865
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-06-06 20:31:16 -0700

    Switched sections

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
       <HeroSection />
       <PodcastDesc />
       <AboutMe />
-      <ContactPage />
       <BibleVerses />
+      <ContactPage />
     </main>
   );
 }
