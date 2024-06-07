b676fb4a913ce5006257f485c26dbead5f6e669f
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-06-07 14:05:25 -0700

    Removed dark classes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -65,7 +65,7 @@ export const FloatingNav = ({
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
-              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+              "relative items-center flex space-x-1 text-cream hover:text-white hover:translate-y-1 hover:scale-105 transition-transform duration-200 ease-in-out"
             )}
           >
             <span className="block sm:hidden">{navItem.icon}</span>
