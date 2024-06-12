9b2299396ecd884b20f72a4c7a589c08f963ade1
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-06-12 13:29:17 -0700

    Add prayer cards

---
+++ components/prayer-card.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { PrayerRequest } from "@prisma/client";
+
+export default function PrayerCard({ prayer }: { prayer: PrayerRequest }) {
+  return (
+    <section className="border-2 w-[350px] p-6 rounded-3xl h-auto flex flex-col justify-between gap-4 bg-pink-tertiary">
+      <h1 className="font-semibold text-gray-400">
+        Prayer Request from: {prayer.name}
+      </h1>
+      <p className="font-light break-words">Prayer Request: {prayer.prayer}</p>
+      <p>
+        Submitted:{" "}
+        {new Date(Date.parse(prayer.createdAt.toString())).toLocaleString(
+          "en-US",
+          {
+            weekday: "long",
+            month: "long",
+            day: "numeric",
+          }
+        )}{" "}
+      </p>
+    </section>
+  );
+}
