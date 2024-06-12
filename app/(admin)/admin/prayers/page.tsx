71a11cc90b60c107f83b748b5ce79172a77ed9aa
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-06-12 13:29:06 -0700

    Updated prayers section

--- app/(admin)/admin/prayers/page.tsx
+++ app/(admin)/admin/prayers/page.tsx
@@ -1,17 +1,21 @@
 import prisma from "@/lib/db";
 
+import PrayerCard from "@/components/prayer-card";
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
+
 export default async function PrayerRequests() {
   const prayers = await prisma.prayerRequest.findMany({});
 
   return (
-    <main>
-      {prayers.map((prayer) => (
-        <div key={prayer.id}>
-          <h1 className="font-bold">Prayer from: {prayer.name}</h1>
-          <p>Prayer Request: {prayer.prayer}</p>
-          <p>Date: {prayer.createdAt.toString()} </p>
-        </div>
-      ))}
+    <main className="flex flex-col items-center gap-20 bg-cream h-screen p-10">
+      <div className="text-center">
+        <TextGenerateEffect words="Prayer Requests" />
+      </div>
+      <div className="grid md:grid-cols-2 gap-y-4 gap-x-6">
+        {prayers.map((prayer) => (
+          <PrayerCard prayer={prayer} key={prayer.id} />
+        ))}
+      </div>
     </main>
   );
 }
