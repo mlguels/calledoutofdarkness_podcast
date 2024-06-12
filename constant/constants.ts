d26fce51ebac9b8ce4ba299c7fd2e4e2fab3b533
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-06-12 13:29:46 -0700

    Fix bible verses

--- constant/constants.ts
+++ constant/constants.ts
@@ -67,16 +67,18 @@ export const bibleVerses = [
   {
     quote:
       '"For ye were sometimes darkness, but now are ye light in the Lord: walk as children of light"',
-    name: "Ephisians: 5:8",
+    name: "Ephisians 5:8",
   },
   {
     quote:
       '"Do not conform to the pattern of this world, but be transformed by the renewing of your mind. Then you will be able to test and approve what God’s will is—his good, pleasing and perfect will."',
-    name: "Romans: 12:2",
+    name: "Romans 12:2",
   },
   {
     quote:
       "“Come to me, all you who are weary and burdened, and I will give you rest. Take my yoke upon you and learn from me, for I am gentle and humble in heart, and you will find rest for your souls. For my yoke is easy and my burden is light.”",
-    name: "Mathew: 11:28-30",
+    name: "Mathew 11:28-30",
   },
 ];
+
+("Matthew 21:22, 7:7, 7:24-27, ps.31:3,John 1:1-5, 1:14, 8:12,8:44, 3:5-7, 3:19-21,Ephesians 5:3-5, 5:14, Romans 12:2, 13:12-13, Colossians 3:8-10, Galatians 5:19-21, Philippians 2:15-16, Isaiah 58:6");
