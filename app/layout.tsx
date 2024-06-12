fa4518ab5321db539fccde6156f39ff94c795227
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-06-12 14:51:25 -0700

    Add anylitics

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className}`}>
-        <Analytics />
         <FloatingNav navItems={navItems} />
         {children}
+        <Analytics />
         <Toaster position="top-right" />
       </body>
     </html>
