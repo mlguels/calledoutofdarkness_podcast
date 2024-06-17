91fef016e1f64fe8a0a131cf85e53a43682ec9df
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-06-16 17:14:24 -0700

    Add email icon

--- components/socials.tsx
+++ components/socials.tsx
@@ -5,11 +5,11 @@ import { lora, socialLinks } from "@/constant/constants";
 
 export default function Socials() {
   return (
-    <main className="flex gap-4">
+    <main className="flex gap-4 items-center">
       <h1 className={`${lora.className} text-3xl`}>Socials</h1>
-      <ul className="flex gap-2">
+      <ul className="flex gap-2 items-center">
         {socialLinks.map((social, i) => (
-          <li key={i}>
+          <li key={i} className="state-effect">
             <Link href={social.link} target="_blank">
               <Image
                 src={social.icon}
@@ -20,6 +20,17 @@ export default function Socials() {
             </Link>
           </li>
         ))}
+        <a
+          href="mailto:calledoutofdarkness.podcast@gmail.com"
+          className="state-effect"
+        >
+          <Image
+            src="/images/envelope.png"
+            alt="email icon"
+            width={45}
+            height={25}
+          />
+        </a>
       </ul>
     </main>
   );
